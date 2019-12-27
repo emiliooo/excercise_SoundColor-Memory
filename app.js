01a3c1352aaf2c390cb9cd1820661f9d60d177e3
Emil <emilburaczynski@gmail.com>
2019-12-27 14:40:46 +0100

    update filed

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
-var min = 0;
-var max = 6;
-
+var min = 0,
+    max = 6,
+    runCount = 0,
+    actionLoop,
+    listsNumbersCard = [],
+    clickedList = [],
+    status = 0;
 
 document.getElementById("btn_start").addEventListener("click", function () {
-    randomBox()
+    runCount = 0;
+    listsNumbersCard = []
+    clickedList = [];
+    status = 0;
+    randomBox();
+});
+
+document.getElementById("btn_check").addEventListener("click", function () {
+    for (var i = 0; i < listsNumbersCard.length; i++) {
+        if (listsNumbersCard[i] === clickedList[i]) {
+            status++
+        }
+    }
+
+    if (status == listsNumbersCard.length && listsNumbersCard.length == clickedList.length) {
+        alert('BRAWO !!!')
+    } else {
+        alert('ZLE !!')
+    }
+});
+
+
+document.getElementById("list_box-card").addEventListener("click", function (event) {
+    console.log(event.target.id);
+    clickedList.push(event.target.id);
+
+    document.getElementById(event.target.id).style.opacity = 2
+    sound();
+    setTimeout(function () {
+        document.getElementById(event.target.id).style.opacity = 0.2
+    }, 1000);
 });
 
 
@@ -16,17 +50,24 @@ function action() {
     setTimeout(function () {
         items[random].style.opacity = 0.2
     }, 1000);
-}
 
-
-var runCount = 0;
-function randomBox() {
-    runCount++;
-    var actionLoop = setInterval(() => {
-        action()
-    }, 2000);
+    listsNumbersCard.push(items[random].id);
+    sound();
 
     if (runCount > 4) {
         clearInterval(actionLoop);
     }
+    console.log(listsNumbersCard)
+}
+
+function randomBox() {
+    actionLoop = setInterval(() => {
+        runCount++;
+        action();
+    }, 2000);
+}
+
+function sound() {
+    var audio = new Audio('./sounds/clay.mp3');
+    audio.play();
 }
